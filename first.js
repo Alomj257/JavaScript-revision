a6da6fd90e74a1dca6669e7e2a58f94464fee617
Jahangir Alom <alomj257@gmail.com>
2025-01-12 18:59:04 +0530

    String methods

--- first.js
+++ first.js
@@ -24,3 +24,49 @@ let obj = {
 }
 
 console.log(`The cost of ${obj.item} is rs ${obj.price}`)
+
+console.log("Jahangir\nAlom");
+let string = "I am jahangir \t alom";
+console.log(string);
+console.log(string.length);
+
+// String methods
+
+let s = "Aasia Bano";
+let s1 = s.toUpperCase();
+let s2 = s.toLocaleLowerCase();
+let s3 = s.trim();
+console.log(s);
+console.log(s1);
+console.log(s2);
+console.log(s3);
+
+let st = "Jahangir Alom    ";
+st = st.toUpperCase();
+console.log(st);
+console.log(st.trim());
+console.log(st.slice(5,8));
+
+
+let strr = "abncde";
+console.log(strr.slice(2,5));
+console.log(strr[5]);
+console.log(strr.charAt(5));
+
+
+let t1 = "Jahangir";
+let t2 = "Aasia";
+let result = t1.concat(t2);
+let ans = t1 + t2;
+console.log(ans);
+console.log(result);
+
+
+let stri = "Hello" + 123 + "Jahangir";
+console.log(stri);
+
+let a = "123" + "Hello";
+console.log(a);
+
+let h = "Hello";
+console.log(h.replace('o','jahangi'));
