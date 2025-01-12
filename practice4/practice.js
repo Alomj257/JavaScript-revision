e4cea7ceaef1de489a59da270781ab4f80a1f0b0
Jahangir Alom <alomj257@gmail.com>
2025-01-12 19:07:30 +0530

    Practice question

---
+++ practice4/practice.js
@@ -0,0 +1,7 @@
+// User full name and generate user name
+
+let str = prompt("Enter your name");
+let userName = "@"+str+str.length;
+let result = `@${str}${str.length}`;
+console.log(userName);
+console.log(result);
