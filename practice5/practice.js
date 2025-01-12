1de7ddb5a04e423467044689274128a91524ad5d
Jahangir Alom <alomj257@gmail.com>
2025-01-12 23:05:53 +0530

    practice q discount

--- practice5/practice.js
+++ practice5/practice.js
@@ -1,4 +1,4 @@
-// Average marks of the entire class
+// // Average marks of the entire class
 
 let arr = [85, 97, 44, 37, 76, 60];
 let sum =0;
@@ -20,3 +20,21 @@ for(let i of arr){
 }
 avg = sum/arr.length;
 console.log(avg)
+
+
+// Change the array items have an offer of 10%;
+
+let items = [250, 645, 300, 900, 50];
+
+// for-of loops
+for(item of items){
+    item = item - item*(10/100);
+    console.log(item)
+}
+
+// for loop
+for (let i =0; i<items.length; i++){
+    items[i] = items[i] - items[i]*(10/100);
+    console.log(items[i])
+}
+console.log(items);
