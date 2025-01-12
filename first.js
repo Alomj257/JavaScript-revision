ba5405781a1671f323d59fc0ff7fc995376ac9aa
Jahangir Alom <alomj257@gmail.com>
2025-01-12 22:03:16 +0530

    array in js part 1

--- first.js
+++ first.js
@@ -1,19 +1,50 @@
-const student = {
-    studentName : "Jahangir Alom",
-    roleNo : 'Ece194008',
-    age: 27,
-    department: "ECE",
-    yop: 2023,
-    isPass: true,
-    isPlaced: true
+// Arrays in js
+// Collection of items in contigious memory allocation.
+
+let heros = ["Jahangir", "ironman", "hulk", "thor"];
+console.log(heros);
+console.log(heros.length);
+
+let marks = [91, 92, 96, 78, 85];
+console.log(marks);
+console.log(typeof marks)
+
+// Special loops in js
+for(let i of heros){
+    console.log(i);
 }
 
-student.studentName = "Aasia Bano";
-student["studentName"] = "Jahangir";
+// Using for loop in js
+for(let i=0; i<heros.length; i++){
+    console.log(heros[i]);
+}
 
+
+// Object in js
+let student = {
+    student1 : 92,
+    student2 : 86,
+    student3: 81,
+    student4: 98,
+    student5: 87
+}
+console.log(student);
+let size = 0;
 for(let key in student){
     console.log(key);
-    console.log(student[key]);
+    console.log(student[key])
+    size = size +1;
+}
+console.log(size)
+
+
+// loops in string 
+let str = "I am jahangir Alom";
+
+for(let i of str){
+    console.log(i);
 }
 
-console.log(student);
+for(let i =0; i<str.length; i++){
+    console.log(str.charAt(i));
+}
