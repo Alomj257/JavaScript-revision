5bc2d4788a8d3ec795c2b911cf068f65dae60d04
Jahangir Alom <alomj257@gmail.com>
2025-01-17 02:36:15 +0530

    practice question 2

--- chapter6/script.js
+++ chapter6/script.js
@@ -1,2 +1,12 @@
 let h2 = document.querySelector("h2");
 h2.innerText = h2.innerText + " from Jahangir Alom";  //Concatenate
+
+
+let div = document.querySelectorAll(".box");
+
+let index =0;
+for(let d of div){
+    console.log(d.innerText)
+    d.innerText = `new unique value ${index}`;
+    index ++;
+}
