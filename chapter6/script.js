3b3d73478a46ec1efff25f9a335e8ed4f29a2034
Jahangir Alom <alomj257@gmail.com>
2025-01-17 02:20:30 +0530

    dom properties

--- chapter6/script.js
+++ chapter6/script.js
@@ -1,14 +1,32 @@
-// DOM Manupulation
+// // DOM Manupulation
 
-// Using Id
-let heading = document.getElementById("heading");
-console.dir(heading);
+// // Using Id
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+// console.log(heading.tagName);
+// console.log(heading.innerText);
+// console.log(heading.innerHTML);
+// console.log(heading.textContent);
 
-// Using ClassName
-let head = document.getElementsByClassName("head");
-console.dir(head);
-console.log(head);
+// // Using ClassName
+// let head = document.getElementsByClassName("head");
+// console.dir(head);
+// console.log(head);
 
-// Using TagName
-let p = document.getElementsByTagName("p");
-console.dir(p);
+// // Using TagName
+// let p = document.getElementsByTagName("p");
+// console.dir(p);
+
+// let firstEl= document.querySelector("p");
+// console.dir(firstEl);
+
+// let allEl = document.querySelectorAll("p");
+// console.dir(allEl)
+
+
+let div = document.querySelector("div");
+console.dir(div)
+
+let heading = document.querySelector("h1");
+heading.innerText = "New heading";
+
