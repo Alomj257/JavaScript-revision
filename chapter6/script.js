d834c0634f34c3cd08796c5e2eee2babeca4af18
Jahangir Alom <alomj257@gmail.com>
2025-01-17 01:51:54 +0530

    dom manupulation

---
+++ chapter6/script.js
@@ -0,0 +1,14 @@
+// DOM Manupulation
+
+// Using Id
+let heading = document.getElementById("heading");
+console.dir(heading);
+
+// Using ClassName
+let head = document.getElementsByClassName("head");
+console.dir(head);
+console.log(head);
+
+// Using TagName
+let p = document.getElementsByTagName("p");
+console.dir(p);
