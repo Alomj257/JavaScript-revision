f30908c5f50d8129bc7370a1b960bebc4f414c1f
Jahangir Alom <alomj257@gmail.com>
2025-01-16 23:42:05 +0530

    map, filter and reduce

--- first.js
+++ first.js
@@ -1,13 +1,42 @@
-// forEcah loops in array
+// More methos in array
 
-let arr = [1,2,3,4,5]
+//map method
 
-arr.forEach((val) => {
-    console.log(val);
+let arr = [1,2,3,4,5];
+
+let newArr = arr.map((val) => {
+    return val*3;
 })
+console.log(newArr)
 
-let city = ["Karimganj", "Kolkata", "Mumbai"];
 
-city.forEach((val, idx, arr) => {
-    console.log(val.toUpperCase(), idx, arr);
+// filter method 
+
+let number = [11,12, 13, 14, 15, 16];
+
+let evenNumber = number.filter((val) => {
+    return val%2 === 0;
 })
+console.log(evenNumber);
+
+let grtNumber = number.filter((val) => {
+    return val >= 15;
+});
+console.log(grtNumber)
+
+// Reduce method
+
+let ar = [1,2,3,4,5,6];
+
+let sum= ar.reduce((res, curr) => {
+    return res + curr;
+});
+let avg = sum/ar.length;
+console.log(avg)
+console.log(sum)
+
+
+let largest = ar.reduce((prev,curr) => {
+    return prev > curr ? prev : curr;
+})
+console.log(largest)
