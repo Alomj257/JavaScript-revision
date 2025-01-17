ef564fd98c6ac3a504ef1d3e34ce1f419c729c8e
Jahangir Alom <alomj257@gmail.com>
2025-01-18 00:03:41 +0530

    event handing

--- chapter8/script.js
+++ chapter8/script.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+let btn1 = document.querySelector("#btn1");
+btn1.onclick = () => {
+    console.log("Btn 1 was clicked");
+    let a = 20;
+    let b = 30;
+    let sum = a + b;
+    sum++;
+    console.log(sum);
+}
+
+let box = document.querySelector("div");
+box.onmouseover = () => {
+    console.log("Mouse hover");
+}
