2c10c29f2fe730edd9a9f6b2ed024e1a9d565414
Jahangir Alom <alomj257@gmail.com>
2025-01-17 22:58:09 +0530

    div.style. attribute

--- chapter6/script.js
+++ chapter6/script.js
@@ -1,3 +1,5 @@
+// get attribute value
+
 let div = document.querySelector("div");
 console.dir(div);
 
@@ -9,3 +11,14 @@ console.dir(para);
 
 let className = para.getAttribute("class");
 console.log(className)
+
+//set attribute value
+
+para.setAttribute("class", "123");
+
+// Acess style
+let box = document.querySelector("#box");
+console.log(div.style)
+div.style.backgroundColor = "red";
+div.style.fontSize = "20px"
+div.innerText = "Hello india"
