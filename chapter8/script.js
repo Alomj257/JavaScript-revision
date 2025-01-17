ed770b39473577635d29eb126925379aa095dff8
Jahangir Alom <alomj257@gmail.com>
2025-01-18 00:32:08 +0530

    practice  question

--- chapter8/script.js
+++ chapter8/script.js
@@ -1,35 +1,14 @@
-let btn1 = document.querySelector("#btn1");
-// btn1.onclick = (e) => {
-//     console.log(e.target)
-//     console.log("Btn 1 was clicked");
-//     let a = 20;
-//     let b = 30;
-//     let sum = a + b;
-//     sum++;
-//     console.log(sum);
-// }
+let btn = document.querySelector("#btn");
 
-// let box = document.querySelector("div");
-// box.onmouseover = () => {
-//     console.log("Mouse hover");
-// }
+let currMode = "light";
 
-//Add event listener
-btn1.addEventListener("click", (e) => {
-    console.log("Button was clicked -hamdler 1");
+btn,addEventListener("click", () => {
+    if(currMode === "light"){
+        currMode = "dark";
+        document.querySelector("body").style.backgroundColor = "black";
+    }else{
+        currMode = "light";
+        document.querySelector("body").style.backgroundColor = "white";
+    }
+    console.log(currMode)
 })
-btn1.addEventListener("click", () => {
-    console.log("Button was clicked - handler 2");
-})
-
-const handler3 = () => {
-    console.log("Button was clicked - handler 3");
-};
-
-btn1.addEventListener("click",handler3 )
-btn1.addEventListener("click", () => {
-    console.log("Button was clicked - handler 4");
-})
-
-// Remove event listner
-btn1.removeEventListener("click", handler3)
