cdc81a237a71859e45afc4b4562a7e8b22a84f00
Jahangir Alom <alomj257@gmail.com>
2025-01-17 23:52:44 +0530

    inline event handing

--- chapter6/script.js
+++ chapter6/script.js
@@ -2,3 +2,4 @@ let para = document.querySelector("p");
 
 // para.setAttribute("class", "newClass"); // override and changes the class name
 para.classList.add("newClass"); // Using class list
+para.classList.remove("para");
