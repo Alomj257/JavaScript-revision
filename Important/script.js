54634eb6aa160d3015f321a6979631fdfd4b5e09
Jahangir Alom <alomj257@gmail.com>
2025-01-25 17:33:03 +0530

    date format and area of triangle

--- Important/script.js
+++ Important/script.js
@@ -1,17 +1,58 @@
-let today = new Date();
-let day = today.getDay();
+// let today = new Date();
+// let day = today.getDay();
 
-let dataList = ["sunday", "monday", "tuesday", "wednesday", "thrusday", "friday", "saturday"];
+// let dataList = ["sunday", "monday", "tuesday", "wednesday", "thrusday", "friday", "saturday"];
 
-console.log("Today day is: ", dataList[day]);
+// console.log("Today day is: ", dataList[day]);
 
-let hr = today.getHours();
-let min = today.getMinutes();
-let sec = today.getSeconds();
-let s = "AM";
-if(hr>12){
-    hr = hr -12;
-    s = "PM"
-}
+// let hr = today.getHours();
+// let min = today.getMinutes();
+// let sec = today.getSeconds();
+// let s = "AM";
+// if(hr>12){
+//     hr = hr -12;
+//     s = "PM"
+// }
 
-console.log("Time is: ", `${hr} ${s}: ${min}: ${sec}`)
+// console.log("Time is: ", `${hr} ${s}: ${min}: ${sec}`)
+
+
+
+
+
+// Current data in variopurs format.
+
+// let today = new Date();
+// console.log(today);
+
+// let date = today.getDate();
+// let month = today.getMonth() + 1;
+// let year = today.getFullYear();
+
+// if(date < 10){
+//     date = "0"+ date;
+// };
+
+// if (month < 10){
+//     month = "0"+ month;
+// }
+
+// var result = month + "/" + date + "/" + year;
+// console.log(result);
+
+// var result = date + "-" + month + "-" + year;
+// console.log(result);
+
+
+
+
+// Calculate the area triabgle;
+
+// let a = parseFloat(prompt("Enter one number"));
+// let b = parseFloat(prompt("Rnter second number"));
+// let c = parseFloat(prompt("enter third number"));
+
+// let s = (a + b + c)/2;
+
+// let area = Math.sqrt(s * (s-a)*(s-b)*(s-c));
+// console.log(area)
