350653f379a9010e148f3ea9f13147764fd70d38
Jahangir Alom <alomj257@gmail.com>
2025-01-30 23:31:47 +0530

    callback

--- chapter12/script.js
+++ chapter12/script.js
@@ -1,15 +1,26 @@
 // Synchoronus and Asynchronous programming
 
-console.log("1");
+// console.log("1");
 
-// function Hello(){
+// // function Hello(){
+// //     console.log("Hello")
+// // }
+// // setTimeout(Hello, 2000)
+
+// setTimeout(() => {
 //     console.log("Hello")
-// }
-// setTimeout(Hello, 2000)
+// }, 2000)
 
-setTimeout(() => {
-    console.log("Hello")
-}, 2000)
+// console.log("2");
+// console.log("3");
 
-console.log("2");
-console.log("3");
+
+// Callbacks
+function sum(a,b){
+    console.log(a+b)
+}
+function calculotor(a,b, sumCallback){
+    sumCallback(a,b)
+}
+
+calculotor(1,2, sum)
