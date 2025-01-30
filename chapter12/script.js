6d2a804700fd77d8c8dd07974719fd6fb47fed6f
Jahangir Alom <alomj257@gmail.com>
2025-01-30 16:35:23 +0530

    async

---
+++ chapter12/script.js
@@ -0,0 +1,15 @@
+// Synchoronus and Asynchronous programming
+
+console.log("1");
+
+// function Hello(){
+//     console.log("Hello")
+// }
+// setTimeout(Hello, 2000)
+
+setTimeout(() => {
+    console.log("Hello")
+}, 2000)
+
+console.log("2");
+console.log("3");
