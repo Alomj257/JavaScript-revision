ccfaee72ef3b5d2bafe78f83074898fdedcad3a1
Jahangir Alom <alomj257@gmail.com>
2025-01-30 15:16:36 +0530

    classes

--- chapter11/script.js
+++ chapter11/script.js
@@ -1,14 +1,38 @@
-const employee = {
-    calcTax(){
-        console.log("Tax rate is 10%")
-    }
-};
+// Prototype
 
-const karanArjun = {
-    salary: 50000,
-    calTax() {
-        console.log("Tax rate is 20%")
-    }
-};
+// const employee = {
+//     calcTax(){
+//         console.log("Tax rate is 10%")
+//     }
+// };
 
-karanArjun.__proto__ = employee;
+// const karanArjun = {
+//     salary: 50000,
+//     calTax() {
+//         console.log("Tax rate is 20%")
+//     }
+// };
+
+// karanArjun.__proto__ = employee;
+
+
+// Classes in JavaScript
+
+class ToyotaCar{
+    start(){
+        console.log("Start")
+    }
+
+    stop() {
+        console.log("Stop")
+    }
+
+    setBrand(brand){
+        this.brandName = brand;
+    }
+}
+
+let fortuner = new ToyotaCar();
+fortuner.setBrand("fortuner")
+let lexus = new ToyotaCar();
+lexus.setBrand("Lexus")
