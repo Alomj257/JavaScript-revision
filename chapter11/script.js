812ffbda8d23715b7c40d34554a3df0dd4c7ab70
Jahangir Alom <alomj257@gmail.com>
2025-01-30 15:55:30 +0530

    inheritance

--- chapter11/script.js
+++ chapter11/script.js
@@ -18,22 +18,67 @@
 
 // Classes in JavaScript
 
-class ToyotaCar{
-    constructor(brand, milage) {
-        console.log("Creating new object")
-        this.brand = brand;
-        this.milage = milage;
-    }
-    start(){
-        console.log("Start")
-    }
+// class ToyotaCar{
+//     constructor(brand, milage) {
+//         console.log("Creating new object")
+//         this.brand = brand;
+//         this.milage = milage;
+//     }
+//     start(){
+//         console.log("Start")
+//     }
 
-    stop() {
-        console.log("Stop")
+//     stop() {
+//         console.log("Stop")
+//     }
+// }
+
+// let fortuner = new ToyotaCar("fortuner", 10);
+// console.log(fortuner);
+// let lexus = new ToyotaCar("Lexus", 20);
+// console.log(lexus)
+
+
+// Inheritance
+
+// class parent {
+//     hello() {
+//         console.log("Hello");
+//     }
+// }
+
+// class Chlid extends parent {
+
+// }
+
+// let obj = new Chlid();
+// console.log(obj.hello())
+
+class Person {
+    constructor(species){
+        this.species = species;
+    }
+    eat(){
+        console.log("Eat")
+    }
+    sleep(){
+        console.log("Sleep")
+    }
+    work(){
+        console.log("Do nothing")
     }
 }
 
-let fortuner = new ToyotaCar("fortuner", 10);
-console.log(fortuner);
-let lexus = new ToyotaCar("Lexus", 20);
-console.log(lexus)
+class Engineer extends Person{
+    work(){
+        console.log("Solve problem and build something")
+    }
+}
+
+let p1 = new Person("Human");
+console.log(p1)
+
+let jahangir = new Engineer();
+console.log(jahangir.work())
+console.log(jahangir.eat())
+console.log(jahangir.sleep())
