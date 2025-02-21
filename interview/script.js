f6cdfd861c1ea444c2fc070c18756d7db4325847
Jahangir Alom <alomj257@gmail.com>
2025-02-21 11:38:29 +0530

    prime number

--- interview/script.js
+++ interview/script.js
@@ -1,13 +1,25 @@
-let arr = [2,4,5,1,7,87,6];
+// let arr = [2,4,5,1,7,87,6];
 
-const smallElemt = (arr) => {
-    let min = arr[0];
-    for(let i =1; i<arr.length; i++){
-        if(arr[i]< min){
-            min = arr[i];
+// const smallElemt = (arr) => {
+//     let min = arr[0];
+//     for(let i =1; i<arr.length; i++){
+//         if(arr[i]< min){
+//             min = arr[i];
+//         }
+//     }
+//     console.log(min);
+// }
+
+// smallElemt(arr);
+
+
+for(let num=2; num<=200; num++){
+    let isPrime = true;
+    for(let i=2; i<num; i++){
+        if(num %i === 0){
+            isPrime = false;
+            break;
         }
     }
-    console.log(min);
+    if(isPrime) console.log(num);
 }
-
-smallElemt(arr);
